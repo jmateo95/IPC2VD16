83d84e54fc364ed80fa6f6e201de5fb8b1b5c63c
Unknown <201313852@ingenieria.usac.edu.gt>
2016-12-06 23:20:17 -0600

    1er comit
    boton para encontrar la solucion 1 a la ecucacion cuadratica

---
+++ src/ipc/Metodos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ipc;
+
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+/**
+ *
+ * @author joubm
+ */
+public class Metodos {
+    public double x1(double a, double b, double c) {
+        double x1;
+        
+        x1= (((-1)*b)+sqrt((b*b)-4*a*c))/(2*a);
+return x1;
+   }
+      
+}
